214b06357c3e8d8c8ac550ded13a77bd05a4a8f1
Yuriy Semyk <ysemyk@moysklad.ru>
2025-11-08 23:59:37 +0300

    Kotlin3-Functions : Задание 4

--- src/main/kotlin/ru/otus/homework/homework/measureTime.kt
+++ src/main/kotlin/ru/otus/homework/homework/measureTime.kt
@@ -1,7 +1,7 @@
 package ru.otus.homework.homework
 
 fun main() {
-    val time = measureTime()
+    val time = measureTime(::longRunningTask)
     println("Measured time: $time ms")
 }
 
@@ -9,6 +9,10 @@ fun longRunningTask() {
     Thread.sleep(1000)
 }
 
-fun measureTime(): Long {
-    TODO("Implement `measureTime`")
+fun measureTime(funcToMeasure: () -> Unit): Long {
+    val started = System.currentTimeMillis()
+    funcToMeasure()
+    val finished = System.currentTimeMillis()
+    val elapsedTime = finished - started
+    return elapsedTime
 }
