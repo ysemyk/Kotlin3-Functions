b194ac286a5df4ef499443d885459159ddb7d0a6
Yuriy Semyk <ysemyk@moysklad.ru>
2025-11-08 23:23:27 +0300

    Kotlin3-Functions : Задание 2

--- src/main/kotlin/ru/otus/homework/homework/concatStrings.kt
+++ src/main/kotlin/ru/otus/homework/homework/concatStrings.kt
@@ -4,6 +4,13 @@ fun main() {
     check("str1 str2 str3" == concatStrings("str1", "str2", "str3"))
 }
 
-fun concatStrings(vararg strings: String): String {
-    TODO("Implement `concatStrings`")
+fun concatStrings(vararg strings: String, separator: Char = ' '): String {
+    val sb = StringBuilder()
+    for ((index, value) in strings.withIndex()) {
+        sb.append(value)
+        if (index<strings.size-1) {
+            sb.append(separator)
+        }
+    }
+    return sb.toString()
 }
