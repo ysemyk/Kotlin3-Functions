a75763d093c88cb7cc944128caa8906501b94846
Yuriy Semyk <ysemyk@moysklad.ru>
2025-11-08 23:15:45 +0300

    Kotlin3-Functions : Задание 1

--- src/main/kotlin/ru/otus/homework/homework/varArgSum.kt
+++ src/main/kotlin/ru/otus/homework/homework/varArgSum.kt
@@ -4,8 +4,15 @@ fun main() {
     check(3 == varArgSum(1, 2))
     check(6 == varArgSum(1, 2, 3))
     check(10 == varArgSum(1, 2, 3, 4))
+    //Ошибка компиляции, если параметров меньше двух
+    //check(10 == varArgSum(1))
 }
 
-fun varArgSum(a1: Int, a2: Int, a3: Int): Int {
-    TODO("Implement `varArgSum`")
+fun varArgSum(a1: Int, a2: Int, vararg a3: Int): Int {
+    val mandatoryPart = a1 + a2
+    var optionalPart = 0
+    for (optionalNumber in a3) {
+        optionalPart += optionalNumber
+    }
+    return mandatoryPart + optionalPart
 }
